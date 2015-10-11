a246d6d857693d187e0bc04e78f56b7cfdda9cfd
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2015-10-11 16:37:13 +0900

    Implement minimum process

--- XamlStylerConsole/Program.cs
+++ XamlStylerConsole/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Mono.Options;
+using XamlStyler.Core;
+using XamlStyler.Core.Options;
 
 namespace XamlStylerConsole
 {
@@ -10,6 +15,22 @@ namespace XamlStylerConsole
     {
         static void Main(string[] args)
         {
+            var inputFilepath = string.Empty;
+
+            var optionSet = new OptionSet
+            {
+                {"i=|input=", "Input Xaml file.", v => inputFilepath = v},
+            };
+            var extra = optionSet.Parse(args);
+
+            var styler = StylerService.CreateInstance(new StylerOptions());
+
+            var inputXamlText = File.ReadAllText(inputFilepath);
+            var outputXamlText = styler.ManipulateTreeAndFormatInput(inputXamlText);
+
+            Console.WriteLine(inputXamlText);
+            Console.WriteLine("---------------------");
+            Console.WriteLine(outputXamlText);
         }
     }
 }
